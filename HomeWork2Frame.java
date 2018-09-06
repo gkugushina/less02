e0185bc4bab140d2bc5162784c2c4935969484df
galya <gkugushina@gmail.com>
2018-09-06 16:50:56 +0300

    version 3.0

--- HomeWork2Frame.java
+++ HomeWork2Frame.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
